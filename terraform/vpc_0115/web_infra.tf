89d2102de065f76ee7d6bddf92dc7d80b325c81d
Koozzi <kchy666@naver.com>
2021-01-18 02:45:18 +0900

    add nat gateway

--- terraform/vpc_0115/web_infra.tf
+++ terraform/vpc_0115/web_infra.tf
@@ -76,3 +76,77 @@ resource "aws_subnet" "second_private_subnet" {
 		Name = "koozzi-private-02"
 	}
 }
+
+resource "aws_eip" "nat_1" {
+	vpc = true
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+resource "aws_eip" "nat_2" {
+	vpc = true
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+	allocation_id = aws_eip.nat_1.id
+
+	subnet_id = aws_subnet.first_subnet.id
+
+	tags = {
+		Name = "NAT-GW-1"
+	}
+}
+
+resource "aws_nat_gateway" "nat_gateway_2" {
+	allocation_id = aws_eip.nat_2.id
+
+	subnet_id = aws_subnet.second_subnet.id
+
+	tags = {
+		Name = "NAT-GW-2"
+	}
+}
+
+resource "aws_route_table" "route_table_private_1" {
+	vpc_id = aws_vpc.main.id
+
+	tags = {
+		Name = "main-private-1"
+	}
+}
+
+resource "aws_route_table" "route_table_private_2" {
+	vpc_id = aws_vpc.main.id
+
+	tags = {
+		Name = "main-private-2"
+	}
+}
+
+resource "aws_route_table_association" "route_table_association_private_1" {
+	subnet_id = aws_subnet.first_private_subnet.id
+	route_table_id = aws_route_table.route_table_private_1.id
+}
+
+resource "aws_route_table_association" "route_table_association_private_2" {
+	subnet_id = aws_subnet.second_private_subnet.id
+	route_table_id = aws_route_table.route_table_private_2.id
+}
+
+resource "aws_route" "private_nat_1" {
+	route_table_id = aws_route_table.route_table_private_1.id
+	destination_cidr_block = "0.0.0.0/0"
+	nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+}
+
+resource "aws_route" "private_nat_2" {
+  route_table_id              = aws_route_table.route_table_private_2.id
+  destination_cidr_block      = "0.0.0.0/0"
+  nat_gateway_id              = aws_nat_gateway.nat_gateway_2.id
+}
